2edf29c48e9572d5ce788684c5b4d25eb58fca5f
Sviatoslav Radchenko <svatikr@gmail.com>
2026-03-14 15:46:03 +0100

    Solve chapter 1 exercise 1.5

--- chapter1/fahrenheit.c
+++ chapter1/fahrenheit.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float fahr, celsius;
-    float lower, upper, step;
-
-    lower = 0;
-    upper = 300;
-    step = 20;
+    int fahr;
 
     printf("Fahrenheit\tCelsius\n");
-
-    celsius = lower;
-    while (celsius <= upper) {
-        fahr = celsius * (9.0/5.0) + 32.0;
-        printf("%3.0f\t\t%6.0f\n", fahr, celsius);
-        celsius = celsius +  step;
-    }
+    
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf("%3d\t\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
